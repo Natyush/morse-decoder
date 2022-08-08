317056c574ff8d73c6bbd4698402227d530d8cae
Natyush <buzjavka@mail.ru>
2022-08-08 15:50:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let string = '';
+    let morse = [];
+
+    let arr = expr.match(/.{1,10}/g);
+    for (let item of arr) {
+      let arrWithout0 = item.replace(/^0+/, '').split(' ');
+       for (let value of arrWithout0) {
+        string = value.replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ');  
+      } 
+      morse.push(string); 
+    }
+
+   let result = '';
+    result = morse.map((item) => {
+    if (item === ' ') {
+        return ' ';
+    } else {
+       return MORSE_TABLE[item];
+    }
+  });
+  return result.join('');
+  }
 
 module.exports = {
     decode
-}
+};
